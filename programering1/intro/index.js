95e647d9a0fc1dde711be551b9d9ced2fd984870
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-09-01 09:39:44 +0200

    error

--- programering1/intro/index.js
+++ programering1/intro/index.js
@@ -1,5 +1,12 @@
 
 // skapar en variable som heter name 
-let name = "linus"
+let name = "linus"//sträng
+let age = 17 // heltal, int
 
-console.log (`hej ${name}`)
+console.log (`hej ${name} du är ${age}`)
+
+if (age >= 18){console.log("du är vuxen")} 
+else {console.log("du är baby")}
+
+let text = document.querySelector("#text1")
+text.innerHTML = ` ${name} är ${age} år gammal"hej"
