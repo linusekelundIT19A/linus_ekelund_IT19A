1a9cd7570a40e9b872cb81d728fbe0a8a467ec07
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-09-03 09:51:36 +0200

    math

--- programering1/intro/index.js
+++ programering1/intro/index.js
@@ -1,12 +1,39 @@
+// skapar en variabel som heter name och tilldelar det värdet "Kokchun"
+let name = "Linus" // sträng
+let age = 17 // heltal, int 
 
-// skapar en variable som heter name 
-let name = "linus"//sträng
-let age = 17 // heltal, int
+console.log(`Hej ${name} du är ${age} år gammal`)
 
-console.log (`hej ${name} du är ${age}`)
+// if-sats - att göra val
+if (age >= 18){console.log("Du är vuxen")} 
+else 
+{console.log("Du är barn")}
 
-if (age >= 18){console.log("du är vuxen")} 
-else {console.log("du är baby")}
+// DOM - Document Object Model
+let text = document.querySelector("#text")
+text.innerHTML = `${name} är ${age} år gammal`
+// skapar variabler för p tagarna 
+let mult = document.querySelector("#multiplikation")
+let div = document.querySelector("#divition")
+let add = document.querySelector("#addtion")
+let sub = document.querySelector("#subtrkation")
 
-let text = document.querySelector("#text1")
-text.innerHTML = ` ${name} är ${age} år gammal"hej"
+mult.innerHTML = `multiplikation`
+div.innerHTML = `divition`
+add.innerHTML = `addtion`
+sub.innerHTML = `subtraktion`
+
+let button = document.querySelector("#knapp")
+
+button.addEventListener("click",Math)
+
+function Math(event){
+console.log("math-funktioner körs")
+
+let num1 = parseFloat(document.querySelector("#number1").value )
+let num2 = parseFloat(document.querySelector("#number2").value )
+
+add.innerHTML = `${num1}+${num2}=${num1+num2}`
+}
+
+
