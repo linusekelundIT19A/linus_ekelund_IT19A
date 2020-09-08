4235314ce81ce6af2924671c3d79f0f6572c0638
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-09-08 09:37:36 +0200

    matte

--- variabler_io/index.js
+++ variabler_io/index.js
@@ -1,1 +1,12 @@
-console.log("hej")
+
+let name = "Linus"
+console.log(`Hej ${name}`)
+let tall = 3 
+let tall2 = 4
+let tall3 = 5
+medelvardet = (tall+tall2+tall3)/3
+medelvardet = `(${tall}+${tall2}+${tall3})/3=${medelvardet}`
+console.log (medelvardet)
+let langd = prompt("ange vädert för att rektangelns längd ") 
+
+button.addEventListener("click",Math)
