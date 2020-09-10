70b14417c83679a52f7a14d0130c429e743353cb
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-09-10 09:20:18 +0200

    upgift

---
+++ upgift/index.js
@@ -0,0 +1,11 @@
+let name = prompt ("vad heter du")
+let age = prompt ("hur gammal")
+let adress = prompt ("vad är din adress")
+let postnumber = prompt ("vad är dit postnumber")
+let stad = prompt ("vilken stad bor du i")
+let telefonnummer = prompt ("Vad är ditt telefonnumme")
+
+console.log (`Name:${name}`)
+console.log (`Ålder:${age}`)
+console.log (`adress:${adress},${postnumber},${stad}`)
+console.log (`telefonnummer${telefonnummer}`)
