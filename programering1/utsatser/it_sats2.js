b46dda2698d838416cbc05b08b74496a932b9d2d
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-09-17 09:47:10 +0200

    wkbenkgejepgijnw

--- programering1/utsatser/it_sats2.js
+++ programering1/utsatser/it_sats2.js
@@ -13,3 +13,9 @@ let age3 = prompt ("Hur gammal är du")
 if (age >= 7){console.log("gå och köp lego")} 
 else 
 {console.log("ät inte lego")}
+
+let price = prompt ("hurmånga gånger ska du åka")
+if (price >=30){
+    console.log("rekomenderar månadskort vid pris av 775kr")}
+    else
+    {console.log("köp enkelbiljett värde av 30kr")}
