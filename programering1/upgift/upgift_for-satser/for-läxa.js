ce17736639a24558dd9c6ce5740565e8d62c8bc1
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-10-01 09:33:29 +0200

    läxa 2

--- programering1/upgift/upgift_for-satser/for-läxa.js
+++ programering1/upgift/upgift_for-satser/for-läxa.js
@@ -1,14 +1,16 @@
 let p_lektioner = document.querySelector("#lektioner")
+let p_tal1 = document.querySelector("#tal1")
+let p_tal2 = document.querySelector("#tal2")
 
 
-
-
-
-
-
-
-
-
+for (let i=2 ; i<=100 ; i++){
+p_tal1.innerHTML +=`${i} `
+i++
+}
+for (let l=1 ; l<=100 ; l++){
+    p_tal2.innerHTML +=`${l} `
+    l++ 
+    }
 
 
 let lektioner = ["Svenska", "Engelska", "Programmering", "Matematik", "Idrott"]
