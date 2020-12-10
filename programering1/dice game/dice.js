d754560ec35ba3fcc3c10c7b69a69690f013ff65
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2020-12-10 09:22:04 +0100

    lol

--- programering1/dice game/dice.js
+++ programering1/dice game/dice.js
@@ -1,26 +1,32 @@
-console.log("holla")
-
-
-for(let j = 0; j<100; j++){
-let dice = Math.floor(Math.random() * 6) + 1;
-console.log (dice)
-}
-
-let dicenumber1 = 0
-let dicenumber2 = 0
+let diceNumber1 = 0
+let diceNumber2 = 0
 let dice1_btn = document.querySelector("#dice_p1")
 let dice2_btn = document.querySelector("#dice_p2")
 let p1_result = document.querySelector("#p1_result")
+let p2_result = document.querySelector("#p2_result")
+let vinnare_btn = document.querySelector("#vinnare")
 
+dice1_btn.addEventListener("click", diceRoll1)
+dice2_btn.addEventListener("click", diceRoll2)
+vinnare_btn.addEventListener("click", vinnare)
 
- function diceRoll1(){
-     dicenumber1 = 1+math.floor(math.random()*6)
-     p1_result.innerHTML = dicenumber1
-     console.log(dicenumber1)
- }
+function diceRoll1(){
+    diceNumber1 = 1+Math.floor(Math.random()*6)
+    p1_result.innerHTML = diceNumber1
+    console.log(diceNumber1)
+}
 
- function diceRoll2(){
-    dicenumber2 = 1+math.floor(math.random()*6)
-    p2_result.innerHTML = dicenumber1
-    console.log(dicenumber1)
+function diceRoll2(){
+    diceNumber2 = Math.floor(Math.random()*6)+1
+    p2_result.innerHTML = diceNumber2
+    console.log(diceNumber2)
+}
+function vinnare(){
+if (diceNumber2 == diceNumber1){
+    console.log ("lika")
+} else if (diceNumber1 < diceNumber2){
+    console.log("spelare 2 vinnare")
+} else if (diceNumber2 < diceNumber1) {
+    console.log("spelare 1 vinnare")
+}
 }
