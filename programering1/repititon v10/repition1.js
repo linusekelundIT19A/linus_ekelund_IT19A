93923d100a256628157a0cc5ce0f47e71b7a4b5f
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2021-03-10 10:04:02 +0100

    Update repition1.js

--- programering1/repititon v10/repition1.js
+++ programering1/repititon v10/repition1.js
@@ -3,17 +3,6 @@ console.log("hej")
 let namn = "Linus"
 let ålder = "17"
 
-console.log(`hej ${namn}, du är ${ålder} år gammal`)
-
-if (ålder >= 18){
-    console.log("du är myndig")
-} else {
-    console.log("du är inte myndig");
-}
-
-for (let i = 0; i < 20; i++){
-    if (i % 2 == 0){
-
-    console.log(i);  
-}
-}
+function tiokompis(kompisett, kompistvå)
+{
+    if(((kompis
