9d7ab400a4ffc1f3b7ca9627e4e3b98bc0ed73e5
linusekelundIT19A <70194221+linusekelundIT19A@users.noreply.github.com>
2021-05-06 13:56:31 +0200

    coolt

--- Slut uppgift/JS/schema.js
+++ Slut uppgift/JS/schema.js
@@ -1,1 +1,13 @@
 console.log("hej")
+document.getElementById("textSection").innerHTML = "kalle";
+function readTextFile() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "bamba.txt", true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4) {
+        var allText = rawFile.responseText;
+        document.getElementById("textSection").innerHTML = allText;
+      }
+    }
+    rawFile.send();
+  }
